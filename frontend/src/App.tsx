ca4c752426998087c6f35cc7f3e174e4f66226df
My Name <yrathi2@buffalo.edu>
2025-03-06 16:40:02 -0500

    set up Tailwind CSS v3 in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      <h1 className="text-2xl">DevPulse Test</h1>
+    </div>
+  );
+}
+export default App;
